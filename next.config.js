be0b40406fb2cab59a3390b8cb92981951eea5dc
Your Name <abhia22442@gmail.com>
2025-10-12 22:52:25 -0400

    .

--- next.config.js
+++ next.config.js
@@ -1,22 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubPages = process.env.NEXT_PUBLIC_GITHUB_PAGES === 'true';
-
-const repo = "personal-website2026";
-const assetPrefix = isGithubPages ? `/${repo}/` : '';
-const basePath = isGithubPages ? `/${repo}` : '';
+const isGithubPages = process.env.NODE_ENV === 'production';
+const repoName = 'personalwebsite';
 
 const nextConfig = {
-    output: 'export',
-    basePath,
-    assetPrefix,
-    images: {
-        unoptimized: true,
-    },
-    env: {
-        NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY: process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY || 'a2bef6ba-ec2c-4592-96eb-30819429ced7',
-        NEXT_PUBLIC_BASE_PATH: basePath,
-    },
+  output: 'export',
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY: process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY || 'a2bef6ba-ec2c-4592-96eb-30819429ced7',
+    NEXT_PUBLIC_BASE_PATH: isGithubPages ? `/${repoName}` : '',
+  },
 };
 
 module.exports = nextConfig;
